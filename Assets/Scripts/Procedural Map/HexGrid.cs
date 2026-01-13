6efb75a5571ecea4ea308eaa31ba79309eb8f649
Filoue <tonarypetitfiloue@gmail.com>
2026-01-13 14:27:54 +0100

    some Triangulation

--- Assets/Scripts/Procedural Map/HexGrid.cs
+++ Assets/Scripts/Procedural Map/HexGrid.cs
@@ -39,10 +39,10 @@ public class HexGrid : MonoBehaviour
 
     void Update()
     {
-        if (Input.GetMouseButtonDown(0))
-        {
-            HandleInput();
-        }
+        // if (Input.GetMouseButtonDown(0))
+        // {
+        //     HandleInput();
+        // }
     }
     void HandleInput()
     {
@@ -50,17 +50,17 @@ public class HexGrid : MonoBehaviour
         RaycastHit hit;
         if (Physics.Raycast(inputRay, out hit))
         {
-            TouchCell(hit.point);
+            ColorCell(hit.point, touchedColor);
         }
     }
 
-    private void TouchCell(Vector3 position)
+    public void ColorCell(Vector3 position, Color color)
     {
         position = transform.InverseTransformPoint(position);
         HexCoordinates coordinates = HexCoordinates.FromPosition(position);
         int index = coordinates.X + coordinates.Z * width + coordinates.Z / 2;
         HexCell cell = cells[index];
-        cell.color = touchedColor;
+        cell.color = color;
         hexMesh.Triangulate(cells);
         Debug.Log("touched at " + coordinates.ToString());
     }
@@ -77,6 +77,31 @@ public class HexGrid : MonoBehaviour
         cell.transform.localPosition = position;
         cell.coordinates = HexCoordinates.FromOffsetCoordinates(x, z);
         cell.color = defaultColor;
+
+        if (x > 0)
+        {
+            cell.SetNeighbor(HexDirection.W, cells[i - 1]);
+        }
+
+        if (z > 0)
+        {
+            if ((z & 1) == 0)
+            {
+                cell.SetNeighbor(HexDirection.SE, cells[i - width]);
+                if (x > 0)
+                {
+                    cell.SetNeighbor(HexDirection.SW, cells[i - width - 1]);
+                }
+            }
+            else
+            {
+                cell.SetNeighbor(HexDirection.SW, cells[i - width]);
+                if (x < width - 1)
+                {
+                    cell.SetNeighbor(HexDirection.SE, cells[i - width + 1]);
+                }
+            }
+        }
         
         Text label = Instantiate<Text>(cellLabelPrefab);
         label.rectTransform.SetParent(gridCanvas.transform, false);
